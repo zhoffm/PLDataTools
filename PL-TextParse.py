06c14092023716d7f1f3b7677de039a4410fb57e
Samuel Srajer <ssrajer@saxonburg.ii-vi.net>
2018-02-01 12:52:17 -0600

    fixed issue where current time was not updating

--- PL-TextParse.py
+++ PL-TextParse.py
@@ -51,15 +51,14 @@ def get_spectral_data(data):
             break
 
 
-current_time = datetime.now().strftime('%Y%m%d%H%M%S')
-
 while True:
     print('\nWelcome to PL-TextParse!\n')
     print('1. Spectral\n2. VCSEL\n3. Exit')
     x = input('Please input your measurement type: ')
 
     if x == '1' or x == 'spectral' or x == 'Spectral':
-        spectral_path = "./Spectral_Text_Files/"
+        current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        spectral_path = "./SPECTRAL_PR03/"
         spectral_datafiles = listdir(spectral_path)
         spectral_data = "SpectralData_" + current_time + ".csv"
         spectral_headers = ["FileName",
@@ -70,18 +69,18 @@ while True:
                             "IntegratedSignal (a.u.)",
                             "FWHM (nm)"]
 
-        g = open(spectral_data, 'a', newline='')
-        gwriter = csv.writer(g)
-        gwriter.writerow(spectral_headers)
-        for i in spectral_datafiles:
-            measdata = [i]
-            get_datetime(spectral_path, i)
-            get_temp(spectral_path, i)
-            get_spectral_data(i)
-            gwriter.writerow(measdata)
-        g.close()
+        with open(spectral_data, 'a', newline='') as g:
+            gwriter = csv.writer(g)
+            gwriter.writerow(spectral_headers)
+            for i in spectral_datafiles:
+                measdata = [i]
+                get_datetime(spectral_path, i)
+                get_temp(spectral_path, i)
+                get_spectral_data(i)
+                gwriter.writerow(measdata)
         print('\nParsing complete. Returning to menu...\n')
     elif x == '2' or x == 'VCSEL' or x == 'vcsel':
+        current_time = datetime.now().strftime('%Y%m%d%H%M%S')
         VCSEL_path = "./VCSEL_Text_Files/"
         VCSEL_datafiles = listdir(VCSEL_path)
         VCSEL_data = "VCSELData_" + current_time + ".csv"
@@ -94,16 +93,15 @@ while True:
                          "SB Delta (nm)"
                          ]
 
-        h = open(VCSEL_data, 'a', newline='')
-        hwriter = csv.writer(h)
-        hwriter.writerow(VCSEL_headers)
-        for i in VCSEL_datafiles:
-            measdata = [i]
-            get_datetime(VCSEL_path, i)
-            get_temp(VCSEL_path, i)
-            get_vcsel_data(i)
-            hwriter.writerow(measdata)
-        h.close()
+        with open(VCSEL_data, 'a', newline='') as h:
+            hwriter = csv.writer(h)
+            hwriter.writerow(VCSEL_headers)
+            for i in VCSEL_datafiles:
+                measdata = [i]
+                get_datetime(VCSEL_path, i)
+                get_temp(VCSEL_path, i)
+                get_vcsel_data(i)
+                hwriter.writerow(measdata)
         print('\nParsing complete. Returning to menu...\n')
     elif x == '3':
         print('Exiting...')
